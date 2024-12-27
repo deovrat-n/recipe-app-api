3cdf889f011d97c829f8feb748afdb8843741123
deovrat-n <deovrat2203@gmail.com>
2024-12-27 06:38:02 +0530

    linting

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-
 # Create your models here.
 """
 Database models.
@@ -22,17 +20,17 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
+
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
+
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
